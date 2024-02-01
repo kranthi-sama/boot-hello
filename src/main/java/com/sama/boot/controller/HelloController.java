1e91fc0fd11963b76c03c1da643fe5c5ec7fef35
Kranthi Sama <sama.kranthi9@gmail.com>
2024-02-01 14:40:50 +0530

    initial

---
+++ src/main/java/com/sama/boot/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.sama.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Map<String,String> helloUser(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        map.put("foo", "bar");
+        map.put("aa", "bb");
+        return map;
+    }
+}
